78e7d5037cea3e4b3e7086a15faa7cdf99e99549
Jannis Harder <me@jix.one>
2020-11-29 15:19:29 +0100

    stop leaking disconnected callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cadical.hpp>
 #include <functional>
+#include <memory>
 #include <optional>
 #include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
@@ -12,6 +13,8 @@ public:
   Solver();
 
   std::optional<std::exception_ptr> py_error;
+  std::unique_ptr<CaDiCaL::Terminator> terminator;
+  std::unique_ptr<CaDiCaL::Learner> learner;
 
   void check_exception() {
     if (py_error) {
@@ -39,7 +42,8 @@ public:
 };
 
 Solver::Solver() : CaDiCaL::Solver() {
-  connect_terminator(new InterruptTerminator(*this));
+  terminator = std::make_unique<InterruptTerminator>(*this);
+  connect_terminator(terminator.get());
 }
 
 class Terminator : public CaDiCaL::Terminator {
@@ -147,235 +151,228 @@ PYBIND11_MODULE(pydical, m) {
         Pydical Python interface for the CaDiCaL SAT solver
     )pbdoc";
 
-  py::class_<Solver>(m, "Solver")
-      .def(py::init())
-      .def_property_readonly_static(
-          "signature", [](py::object) { return Solver::signature(); })
-      .def("add", &Solver::add)
-      .def("add_clause",
-          [](Solver &self, py::iterable it) {
-            for (py::iterator::reference lit : it) {
-              self.add(lit.cast<int>());
-            }
-            self.add(0);
-          })
-      .def("extend",
-          [](Solver &self, py::iterable it) {
-            for (py::iterator::reference clause : it) {
-              for (py::iterator::reference lit : *clause) {
-                self.add(lit.cast<int>());
-              }
-              self.add(0);
-            }
-          })
-      .def("assume", &Solver::assume)
-      .def("solve",
-          [](Solver &self) {
-            int result = self.solve();
-            self.check_exception();
-            return result;
-          })
-      .def("val", &Solver::val)
-      .def("failed", &Solver::failed)
-      .def("connect_interrupt_terminator",
-          [](Solver &self, std::function<bool()> callback) {
-            self.connect_terminator(new InterruptTerminator(self));
-          })
-      .def("connect_terminator",
-          [](Solver &self, std::function<bool()> callback) {
-            self.connect_terminator(new Terminator(self, std::move(callback)));
-          })
-      .def("disconnect_terminator", &Solver::disconnect_terminator)
-      .def("connect_learner",
-          [](Solver &self, std::function<bool(int)> learning_callback,
-              std::function<void(int)> learn_callback) {
-            self.connect_learner(new Learner(
-                self, std::move(learning_callback), std::move(learn_callback)));
-          })
-      .def("disconnect_learner", &Solver::disconnect_learner)
-      .def("lookahead",
-          [](Solver &self) {
-            int result = self.lookahead();
-            self.check_exception();
-            return result;
-          })
-      .def("generate_cubes", [](Solver &self, int depth) {
-        Solver::CubesWithStatus result = self.generate_cubes(depth);
-        // Terminating this doesn't work, but at least we can report the
-        // exception at the end
+  auto cls = py::class_<Solver>(m, "Solver");
+
+  cls.def(py::init());
+  cls.def_property_readonly_static(
+      "signature", [](py::object) { return Solver::signature(); });
+  cls.def("add", &Solver::add);
+  cls.def("add_clause", [](Solver &self, py::iterable it) {
+    for (py::iterator::reference lit : it) {
+      self.add(lit.cast<int>());
+    }
+    self.add(0);
+  });
+  cls.def("extend", [](Solver &self, py::iterable it) {
+    for (py::iterator::reference clause : it) {
+      for (py::iterator::reference lit : *clause) {
+        self.add(lit.cast<int>());
+      }
+      self.add(0);
+    }
+  });
+  cls.def("assume", &Solver::assume);
+  cls.def("solve", [](Solver &self) {
+    int result = self.solve();
+    self.check_exception();
+    return result;
+  });
+  cls.def("val", &Solver::val);
+  cls.def("failed", &Solver::failed);
+  cls.def("connect_interrupt_terminator",
+      [](Solver &self, std::function<bool()> callback) {
+        self.disconnect_terminator();
+        self.terminator = std::make_unique<InterruptTerminator>(self);
+        self.connect_terminator(self.terminator.get());
+      });
+  cls.def("connect_terminator", [](Solver &self,
+                                    std::function<bool()> callback) {
+    self.disconnect_terminator();
+    self.terminator = std::make_unique<Terminator>(self, std::move(callback));
+    self.connect_terminator(self.terminator.get());
+  });
+  cls.def("disconnect_terminator", [](Solver &self) {
+    self.disconnect_terminator();
+    self.terminator = nullptr;
+  });
+  cls.def("connect_learner",
+      [](Solver &self, std::function<bool(int)> learning_callback,
+          std::function<void(int)> learn_callback) {
+        self.disconnect_learner();
+        self.learner = nullptr;
+        self.connect_learner(new Learner(
+            self, std::move(learning_callback), std::move(learn_callback)));
+      });
+  cls.def("disconnect_learner", [](Solver &self) {
+    self.disconnect_learner();
+    self.learner = nullptr;
+  });
+  cls.def("lookahead", [](Solver &self) {
+    int result = self.lookahead();
+    self.check_exception();
+    return result;
+  });
+  cls.def("generate_cubes", [](Solver &self, int depth) {
+    Solver::CubesWithStatus result = self.generate_cubes(depth);
+    // Terminating this doesn't work, but at least we can report the
+    // exception at the end
+    self.check_exception();
+    return std::make_pair(result.status, result.cubes);
+  });
+  cls.def("reset_assumptions", &Solver::reset_assumptions);
+  cls.def_property_readonly("state", [](Solver &self) {
+    switch (self.state()) {
+    case CaDiCaL::INITIALIZING:
+      return "INITIALIZING";
+    case CaDiCaL::CONFIGURING:
+      return "CONFIGURING";
+    case CaDiCaL::UNKNOWN:
+      return "UNKNOWN";
+    case CaDiCaL::ADDING:
+      return "ADDING";
+    case CaDiCaL::SOLVING:
+      return "SOLVING";
+    case CaDiCaL::SATISFIED:
+      return "SATISFIED";
+    case CaDiCaL::UNSATISFIED:
+      return "UNSATISFIED";
+    case CaDiCaL::DELETING:
+      return "DELETING";
+    default:
+      return "";
+    }
+  });
+  cls.def_property_readonly("status", &Solver::status);
+  cls.def_property_readonly_static(
+      "version", [](py::object) { return Solver::version(); });
+  cls.def("copy", &Solver::copy);
+  cls.def("copy", [](Solver &self) {
+    Solver *copy = new Solver;
+    self.copy(*copy);
+    return copy;
+  });
+  cls.def_property_readonly("vars", &Solver::vars);
+  cls.def("reserve", &Solver::reserve);
+  cls.def_static("is_valid_option", &Solver::is_valid_option);
+  cls.def_static("is_preprocessing_option", &Solver::is_preprocessing_option);
+  cls.def_static("is_valid_long_option", &Solver::is_valid_long_option);
+  cls.def("get", &Solver::get);
+  cls.def("prefix", &Solver::prefix);
+  cls.def("set", &Solver::set);
+  cls.def("set_long_option", &Solver::set_long_option);
+  cls.def_static("is_valid_configuration", &Solver::is_valid_configuration);
+  cls.def("configure", &Solver::configure);
+  cls.def("optimize", &Solver::optimize);
+  cls.def("limit", &Solver::limit);
+  cls.def("is_valid_limit", &Solver::is_valid_limit);
+  cls.def_property_readonly("active", &Solver::active);
+  cls.def_property_readonly("redundant", &Solver::redundant);
+  cls.def_property_readonly("irredundant", &Solver::irredundant);
+  cls.def("simplify", [](Solver &self, int rounds) {
+    int result = self.simplify(rounds);
+    self.check_exception();
+    return result;
+  });
+  cls.def("simplify", [](Solver &self) {
+    int result = self.simplify();
+    self.check_exception();
+    return result;
+  });
+  cls.def("terminate", &Solver::terminate);
+  cls.def("frozen", &Solver::frozen);
+  cls.def("freeze", &Solver::freeze);
+  cls.def("melt", &Solver::melt);
+  cls.def("fixed", &Solver::fixed);
+  cls.def("phase", &Solver::phase);
+  cls.def("unphase", &Solver::unphase);
+  cls.def("trace_proof", [](Solver &self, FILE *file, const char *name) {
+    return self.trace_proof(file, name);
+  });
+  cls.def("trace_proof",
+      [](Solver &self, const char *name) { return self.trace_proof(name); });
+  cls.def("flush_proof_trace", &Solver::flush_proof_trace);
+  cls.def("close_proof_trace", &Solver::close_proof_trace);
+  cls.def_static("usage", &Solver::usage);
+  cls.def_static("configurations", &Solver::configurations);
+  cls.def("statistics", &Solver::statistics);
+  cls.def("resources", &Solver::resources);
+  cls.def("options", &Solver::options);
+  cls.def("traverse_clauses",
+      [](Solver &self, std::function<bool(const std::vector<int> &)> callback) {
+        ClauseIterator it(self, std::move(callback));
+        bool result = self.traverse_clauses(it);
         self.check_exception();
-        return std::make_pair(result.status, result.cubes);
-      })
-      .def("reset_assumptions", &Solver::reset_assumptions)
-      .def_property_readonly("state",
-          [](Solver &self) {
-            switch (self.state()) {
-            case CaDiCaL::INITIALIZING:
-              return "INITIALIZING";
-            case CaDiCaL::CONFIGURING:
-              return "CONFIGURING";
-            case CaDiCaL::UNKNOWN:
-              return "UNKNOWN";
-            case CaDiCaL::ADDING:
-              return "ADDING";
-            case CaDiCaL::SOLVING:
-              return "SOLVING";
-            case CaDiCaL::SATISFIED:
-              return "SATISFIED";
-            case CaDiCaL::UNSATISFIED:
-              return "UNSATISFIED";
-            case CaDiCaL::DELETING:
-              return "DELETING";
-            default:
-              return "";
-            }
-          })
-      .def_property_readonly("status", &Solver::status)
-      .def_property_readonly_static(
-          "version", [](py::object) { return Solver::version(); })
-      .def("copy", &Solver::copy)
-      .def("copy",
-          [](Solver &self) {
-            Solver *copy = new Solver;
-            self.copy(*copy);
-            return copy;
-          })
-      .def_property_readonly("vars", &Solver::vars)
-      .def("reserve", &Solver::reserve)
-      .def_static("is_valid_option", &Solver::is_valid_option)
-      .def_static("is_preprocessing_option", &Solver::is_preprocessing_option)
-      .def_static("is_valid_long_option", &Solver::is_valid_long_option)
-      .def("get", &Solver::get)
-      .def("prefix", &Solver::prefix)
-      .def("set", &Solver::set)
-      .def("set_long_option", &Solver::set_long_option)
-      .def_static("is_valid_configuration", &Solver::is_valid_configuration)
-      .def("configure", &Solver::configure)
-      .def("optimize", &Solver::optimize)
-      .def("limit", &Solver::limit)
-      .def("is_valid_limit", &Solver::is_valid_limit)
-      .def_property_readonly("active", &Solver::active)
-      .def_property_readonly("redundant", &Solver::redundant)
-      .def_property_readonly("irredundant", &Solver::irredundant)
-      .def("simplify",
-          [](Solver &self, int rounds) {
-            int result = self.simplify(rounds);
-            self.check_exception();
-            return result;
-          })
-      .def("simplify",
-          [](Solver &self) {
-            int result = self.simplify();
-            self.check_exception();
-            return result;
-          })
-      .def("terminate", &Solver::terminate)
-      .def("frozen", &Solver::frozen)
-      .def("freeze", &Solver::freeze)
-      .def("melt", &Solver::melt)
-      .def("fixed", &Solver::fixed)
-      .def("phase", &Solver::phase)
-      .def("unphase", &Solver::unphase)
-      .def("trace_proof",
-          [](Solver &self, FILE *file, const char *name) {
-            return self.trace_proof(file, name);
-          })
-      .def("trace_proof",
-          [](Solver &self, const char *name) { return self.trace_proof(name); })
-      .def("flush_proof_trace", &Solver::flush_proof_trace)
-      .def("close_proof_trace", &Solver::close_proof_trace)
-      .def_static("usage", &Solver::usage)
-      .def_static("configurations", &Solver::configurations)
-      .def("statistics", &Solver::statistics)
-      .def("resources", &Solver::resources)
-      .def("options", &Solver::options)
-      .def("traverse_clauses",
-          [](Solver &self,
-              std::function<bool(const std::vector<int> &)> callback) {
-            ClauseIterator it(self, std::move(callback));
-            bool result = self.traverse_clauses(it);
-            self.check_exception();
-            return result;
-          })
-      .def("clauses",
-          [](Solver &self) {
-            CollectingClauseIterator it;
-            self.traverse_clauses(it);
-            return it.clauses;
-          })
-      .def("traverse_witnesses_backward",
-          [](Solver &self, std::function<bool(const std::vector<int> &,
-                               const std::vector<int> &)>
-                               callback) {
-            WitnessIterator it(self, std::move(callback));
-            bool result = self.traverse_witnesses_backward(it);
-            self.check_exception();
-            return result;
-          })
-      .def("traverse_witnesses_forward",
-          [](Solver &self, std::function<bool(const std::vector<int> &,
-                               const std::vector<int> &)>
-                               callback) {
-            WitnessIterator it(self, std::move(callback));
-            bool result = self.traverse_witnesses_forward(it);
-            self.check_exception();
-            return result;
-          })
-      .def("witnesses",
-          [](Solver &self) {
-            CollectingWitnessIterator it;
-            self.traverse_witnesses_forward(it);
-            return it.clauses;
-          })
-      .def("read_dimacs",
-          [](Solver &self, FILE *file, const char *name, int strict) {
-            int vars = -1;
-            const char *msg = self.read_dimacs(file, name, vars, strict);
-            return std::make_pair(msg, vars);
-          })
-      .def("read_dimacs",
-          [](Solver &self, FILE *file, const char *name) {
-            int vars = -1;
-            const char *msg = self.read_dimacs(file, name, vars, 1);
-            return std::make_pair(msg, vars);
-          })
-      .def("read_dimacs",
-          [](Solver &self, const char *name, int strict) {
-            int vars = -1;
-            const char *msg = self.read_dimacs(name, vars, strict);
-            return std::make_pair(msg, vars);
-          })
-      .def("read_dimacs",
-          [](Solver &self, const char *name) {
-            int vars = -1;
-            const char *msg = self.read_dimacs(name, vars, 1);
-            return std::make_pair(msg, vars);
-          })
-      .def("read_dimacs_inccnf",
-          [](Solver &self, const char *name, int strict) {
-            int vars = -1;
-            bool incremental = false;
-            std::vector<int> cubes;
-            const char *msg = self.read_dimacs(name, vars, strict, incremental, cubes);
-            return std::make_tuple(msg, vars, incremental, cubes);
-          })
-      .def("read_dimacs_inccnf",
-          [](Solver &self, const char *name) {
-            int vars = -1;
-            bool incremental = false;
-            std::vector<int> cubes;
-            const char *msg = self.read_dimacs(name, vars, 1, incremental, cubes);
-            return std::make_tuple(msg, vars, incremental, cubes);
-          })
-      .def("write_dimacs", &Solver::write_dimacs)
-      .def("write_dimacs",
-          [](Solver &self,
-              const char *path) { return self.write_dimacs(path); })
-      .def("write_extension", &Solver::write_extension)
-      .def_static("build", &Solver::build)
-      .def_static("build", [](py::object, FILE *file) { Solver::build(file); })
-      /**/
-      ;
+        return result;
+      });
+  cls.def("clauses", [](Solver &self) {
+    CollectingClauseIterator it;
+    self.traverse_clauses(it);
+    return it.clauses;
+  });
+  cls.def("traverse_witnesses_backward",
+      [](Solver &self, std::function<bool(
+                           const std::vector<int> &, const std::vector<int> &)>
+                           callback) {
+        WitnessIterator it(self, std::move(callback));
+        bool result = self.traverse_witnesses_backward(it);
+        self.check_exception();
+        return result;
+      });
+  cls.def("traverse_witnesses_forward",
+      [](Solver &self, std::function<bool(
+                           const std::vector<int> &, const std::vector<int> &)>
+                           callback) {
+        WitnessIterator it(self, std::move(callback));
+        bool result = self.traverse_witnesses_forward(it);
+        self.check_exception();
+        return result;
+      });
+  cls.def("witnesses", [](Solver &self) {
+    CollectingWitnessIterator it;
+    self.traverse_witnesses_forward(it);
+    return it.clauses;
+  });
+  cls.def("read_dimacs",
+      [](Solver &self, FILE *file, const char *name, int strict) {
+        int vars = -1;
+        const char *msg = self.read_dimacs(file, name, vars, strict);
+        return std::make_pair(msg, vars);
+      });
+  cls.def("read_dimacs", [](Solver &self, FILE *file, const char *name) {
+    int vars = -1;
+    const char *msg = self.read_dimacs(file, name, vars, 1);
+    return std::make_pair(msg, vars);
+  });
+  cls.def("read_dimacs", [](Solver &self, const char *name, int strict) {
+    int vars = -1;
+    const char *msg = self.read_dimacs(name, vars, strict);
+    return std::make_pair(msg, vars);
+  });
+  cls.def("read_dimacs", [](Solver &self, const char *name) {
+    int vars = -1;
+    const char *msg = self.read_dimacs(name, vars, 1);
+    return std::make_pair(msg, vars);
+  });
+  cls.def("read_dimacs_inccnf", [](Solver &self, const char *name, int strict) {
+    int vars = -1;
+    bool incremental = false;
+    std::vector<int> cubes;
+    const char *msg = self.read_dimacs(name, vars, strict, incremental, cubes);
+    return std::make_tuple(msg, vars, incremental, cubes);
+  });
+  cls.def("read_dimacs_inccnf", [](Solver &self, const char *name) {
+    int vars = -1;
+    bool incremental = false;
+    std::vector<int> cubes;
+    const char *msg = self.read_dimacs(name, vars, 1, incremental, cubes);
+    return std::make_tuple(msg, vars, incremental, cubes);
+  });
+  cls.def("write_dimacs", &Solver::write_dimacs);
+  cls.def("write_dimacs",
+      [](Solver &self, const char *path) { return self.write_dimacs(path); });
+  cls.def("write_extension", &Solver::write_extension);
+  cls.def_static("build", &Solver::build);
+  cls.def_static("build", [](py::object, FILE *file) { Solver::build(file); });
 
 #ifdef VERSION_INFO
   m.attr("__version__") = VERSION_INFO;
